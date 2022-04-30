1dc27d4bf6fe4f0fb24659ccfe86bca90702cc23
GABRIEL VASCONCELOS VALE <gabriel.vale@alunos.sc.senac.br>
2022-04-29 20:09:34 -0400

    Adicionar arquivos de projeto.

---
+++ Vogais/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Vogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra de A a Z");
+            char letra = char.Parse(Console.ReadLine());
+
+            char a = 'a';
+            char e = 'e';
+            char i = 'i';
+            char o = 'o';
+            char u = 'u';
+
+            
+
+            if (letra == a)
+            {
+                Console.WriteLine("Você digitou uma vogal.");
+
+            }
+            else if(letra == e)
+            {
+                Console.WriteLine("Você digitou uma vogal.");
+            }
+            else if (letra == i)
+            {
+                Console.WriteLine("Você digitou uma vogal.");
+            }
+            else if (letra == o)
+            {
+                Console.WriteLine("Você digitou uma vogal.");
+            }
+            else if (letra == u)
+            {
+                Console.WriteLine("Você digitou uma vogal.");
+            }
+            else
+            {
+                Console.WriteLine("Você digitou uma Consoante.");
+            }
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
